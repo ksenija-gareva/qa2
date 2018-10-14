695328fb9848f931ccbd4e9b665721c7f71373c4
ksenija-gareva <k.gareva@gmail.com>
2018-10-14 13:28:47 +0300

    Loan Expenses calculator. Changes

--- src/test/java/LoanExpenses.java
+++ src/test/java/LoanExpenses.java
@@ -1,11 +1,10 @@
-import java.text.NumberFormat;
-import java.util.Scanner;
+import org.junit.jupiter.api.Assertions;
 
 public class LoanExpenses {
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
 
-        double loanAmount = 80000.0;
+        double loanAmount = 60000.0;
         int termInYears = 10;
 
         double firstRate = 0.1;
@@ -13,111 +12,82 @@ public class LoanExpenses {
         double lastRate = 0.06;
 
 
-        double firstYearPayment = loanAmount / (termInYears * 3);
-        double firstTenYearsPercentage = loanAmount * firstRate / 100;
-        double firstTenYearsBalance = loanAmount - firstYearPayment * 10 - firstTenYearsPercentage;
+        double oneYearPayment = loanAmount / (termInYears * 3);
+        // = 2000.0
+        double firstTenYearsPercentage = loanAmount * firstRate;
+        // = 6000.0
+        double firstTenYearsPayment = oneYearPayment * 10 + firstTenYearsPercentage;
+        // = 26000.0
+        double firstTenYearsBalance = loanAmount - oneYearPayment * 10;
+        // = 40000.0
 
-        double secondOneYearPayment = firstTenYearsBalance / termInYears;
-        double secondTenYearsPercentage = firstTenYearsBalance * secondRate / 100;
-        double secondTenYearsBalance = firstTenYearsBalance - secondOneYearPayment * 10 - secondTenYearsPercentage;
 
-        double lastOneYearPayment = secondTenYearsBalance / termInYears;
-        double lastTenYearsPercentage = secondTenYearsBalance * lastRate / 100;
-        double lastTenYearsBalance = secondTenYearsBalance - lastOneYearPayment * 10 - lastTenYearsPercentage;
+        double secondTenYearsPercentage = firstTenYearsBalance * secondRate;
+        // = 3200.0
+        double secondTenYearsPayment = oneYearPayment * 10 + secondTenYearsPercentage;
+        // = 23200.0
+        double secondTenYearsBalance = firstTenYearsBalance - oneYearPayment * 10;
+        // = 20000.0
+
+
+        double lastTenYearsPercentage = secondTenYearsBalance * lastRate;
+        // = 1200.0
+        double lastTenYearsPayment = oneYearPayment * 10 + lastTenYearsPercentage;
+        // = 21200.0
+        double lastTenYearsBalance = secondTenYearsBalance - oneYearPayment * 10;
+        // = 0.0
+
+        double allYearsPercentageSum = firstTenYearsPercentage + secondTenYearsPercentage + lastTenYearsPercentage;
 
 
         System.out.println("Loan amount is: " + loanAmount);
-        System.out.println("One year payment for first ten years is: " + firstYearPayment);
+        System.out.println("One year payment for first ten years is: " + oneYearPayment);
         System.out.println("First ten years percentage is: " + firstTenYearsPercentage);
         System.out.println("First ten years balance is: " + firstTenYearsBalance);
-        System.out.println("One year payment for second ten years is: " + secondOneYearPayment);
         System.out.println("Second ten years percentage is: " + secondTenYearsPercentage);
         System.out.println("Second ten years balance is: " + secondTenYearsBalance);
-        System.out.println("One year payment for last ten years is: " + lastOneYearPayment);
         System.out.println("Last ten years percentage is: " + lastTenYearsPercentage);
         System.out.println("Last ten years balance is: " + lastTenYearsBalance);
+        System.out.println();
+        System.out.println("First ten years payment with percentage is: " + firstTenYearsPayment);
+        System.out.println("Second ten years payment with percentage is: " + secondTenYearsPayment);
+        System.out.println("Last ten years payment with percentage is: " + lastTenYearsPayment);
+        System.out.println("Percentage sum for all years: " + allYearsPercentageSum);
+
+        Assertions.assertEquals(loanAmount, firstTenYearsBalance + secondTenYearsBalance + lastTenYearsBalance, "The loan is calculated wrong!");
+        Assertions.assertEquals(10400.0, allYearsPercentageSum);
     }
-
-
 }
 
-//public class LoanExpenses {
+//    public static void main (String [] args) {
 //
-//    public static double calculateMonthlyPayment(
-//            int loanAmount, int termInYears, double interestRate) {
+//        double loanAmount = 80000.0;
+//        Integer termInYears = 10;
 //
-//        // Convert interest rate into a decimal
-//        // eg. 6.5% = 0.065
-//
-//        interestRate /= 100.0;
-//
-//        // Monthly interest rate
-//        // is the yearly rate divided by 12
-//
-//        double monthlyRate = interestRate / 12.0;
-//
-//        // The length of the term in months
-//        // is the number of years times 12
-//
-//        int termInMonths = termInYears * 12;
-//
-//        // Calculate the monthly payment
-//        // Typically this formula is provided so
-//        // we won't go into the details
-//
-//        // The Math.pow() method is used calculate values raised to a power
-//
-//        double monthlyPayment =
-//                (loanAmount*monthlyRate) /
-//                        (1-Math.pow(1+monthlyRate, -termInMonths));
-//
-//        return monthlyPayment;
-//    }
-//
-//    public static void main(String[] args) {
-//
-//        // Scanner is a great class for getting
-//        // console input from the user
-//
-//        Scanner scanner = new Scanner(System.in);
-//
-//        // Prompt user for details of loan
-//
-//        System.out.print("Enter loan amount: ");
-//        int loanAmount = scanner.nextInt();
-//
-//        System.out.print("Enter loan term (in years): ");
-//        int termInYears = scanner.nextInt();
-//
-//        System.out.print("Enter interest rate: ");
-//        double interestRate = scanner.nextDouble();
-//
-//        // Display details of loan
-//
-//        double monthlyPayment =
-//                calculateMonthlyPayment(loanAmount, termInYears, interestRate);
+//        Scanner scn = new Scanner(System.in);
 //
 //
-//        // NumberFormat is useful for formatting numbers
-//        // In our case we'll use it for
-//        // formatting currency and percentage values
+//        PercentageRate a = new PercentageRate();
+//       // double a = loanAmount / 0.1;
 //
-//        NumberFormat currencyFormat =
-//                NumberFormat.getCurrencyInstance();
-//        NumberFormat interestFormat =
-//                NumberFormat.getPercentInstance();
 //
-//        // Display details of the loan
+//        PercentageRate b = new PercentageRate();
+//      //  double b = loanAmount / 0.08;
 //
-//        System.out.println("Loan Amount: "+
-//                currencyFormat.format(loanAmount));
-//        System.out.println("Loan Term: "+
-//                termInYears+" years");
-//        System.out.println("Interest Rate: "+
-//                interestFormat.format(interestRate));
-//        System.out.println("Monthly Payment: "+
-//                currencyFormat.format(monthlyPayment));
+//        PercentageRate c = new PercentageRate();
+//      //  double c = loanAmount / 0.06;
 //
-//    }
 //
-//}
+//       double firstTenYearsPercentageSum = loanAmount / (termInYears * 3)) * a / 100;
+//       double secondTenYearsPercentageSum = (loanAmount / termInYears) * b / 100;
+//       double lastTenYearsPercentageSum = (loanAmount / termInYears)  * c / 100;
+//
+//
+//        System.out.println(firstTenYearsPercentageSum);
+//        System.out.println(secondTenYearsPercentageSum);
+//        System.out.println(lastTenYearsPercentageSum);
+
+
+
+
+
